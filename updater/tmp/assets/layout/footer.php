3e56def2cf1a96e101218f98feb0b911cff2e25f
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-01-17 01:24:22 -0600

    ff

--- updater/tmp/assets/layout/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!MediaSettings::isTrue('NO_NAV')) { ?>
-<p>&nbsp;</p>
-</main>
-
-<?php
-}
-
-Footer::display();
--- updater/tmp/test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require_once(".config.inc.php");
-define('TITLE', "Test Page");
-$template = new Template();
-require __LAYOUT_HEADER__;
-
-?>
