74455f1800371c409a64cf0b97f1a2030497000e
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-07-18 14:05:57 -0500

    stuff again

--- cwp/assets/configuration/sqllite/cwp_table_settings.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "settings" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"setting_name" TEXT NOT NULL,
-	"setting_value"	TEXT DEFAULT NULL,
-	"setting_type"	TEXT NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);
--- cwp/home.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once(".config.inc.php");
-header("Location:  ".__URL_PATH__ . "/index.php");
-exit();
