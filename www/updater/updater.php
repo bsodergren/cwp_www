7d41fa38bd841a5177a1989ee3a175bb426ec567
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-07-20 10:09:08 -0500

    ff

--- www/updater/updater.php
+++ www/updater/updater.php
@@ -23,43 +23,54 @@ function get_content($URL)
 
 use Symfony\Component\Process\Process;
 
-$gitRaw           = 'https://raw.githubusercontent.com/bsodergren/cwp_www/main/www/updater/';
-$updateUrl        = $gitRaw.'current.txt?432=432';
-$versionList      = $gitRaw.'version.txt?432=432';
-$zip_url          = $gitRaw.'versions/';
+$gitRaw             = 'https://raw.githubusercontent.com/bsodergren/cwp_www/main/www/updater/';
+$updateUrl          = $gitRaw.'current.txt?432=432';
+$versionUrl         = $gitRaw.'version.txt?432=432';
+$zip_url            = $gitRaw.'versions/';
 
-$builder_exec     = __ROOT_BIN_DIR__.\DIRECTORY_SEPARATOR.'builder.exe';
-$patcher_exec     = __ROOT_BIN_DIR__.\DIRECTORY_SEPARATOR.'patcher.exe';
+$builder_exec       = __ROOT_BIN_DIR__.\DIRECTORY_SEPARATOR.'builder.exe';
+$patcher_exec       = __ROOT_BIN_DIR__.\DIRECTORY_SEPARATOR.'patcher.exe';
 
 define('TITLE', 'Media Updater');
 include_once __LAYOUT_HEADER__;
 
-$version          = trim(get_content($updateUrl));
-$current          = trim(file_get_contents(__VERSION_FILE__));
+$current            = trim(get_content($updateUrl));
+$installed          = trim(file_get_contents(__VERSION_FILE__));
 
-dump([$updateUrl, $version, $current]);
+dump(['Current' => $current, 'Installed' => $installed]);
 
-if ($version > $current) {
+if ($current > $installed) {
     echo 'time to update date '.$patcher_exec;
 
-    $allVersions         = trim(file_get_contents($versionList));
-    dd($allVersions);
-
-    $data                = get_content($zip_url);
-
-    if (!is_dir(__VERSION_DL_DIR__)) {
-        mkdir(__VERSION_DL_DIR__, 0777, true);
+    $allVersions         = get_content($versionUrl);
+    $verArray            = explode("\n", $allVersions);
+    foreach ($verArray as $Updates) {
+        if ($installed >= $Updates) {
+            continue;
+        }
+        $doUpdates[]    = $Updates;
     }
 
-    $destination         = __VERSION_DL_DIR__.\DIRECTORY_SEPARATOR.'Latest.zip';
+    foreach ($doUpdates as $version) {
+        $zip_filename        = 'update_'.$version.'.zip';
+        $zip_url             = $zip_url.$zip_filename;
+        $data                = get_content($zip_url);
 
-    if (file_exists($destination)) {
-        unlink($destination);
+        if (!is_dir(__VERSION_DL_DIR__)) {
+            mkdir(__VERSION_DL_DIR__, 0777, true);
+        }
+
+        $destination         = __VERSION_DL_DIR__.\DIRECTORY_SEPARATOR.zip_filename;
+
+        if (file_exists($destination)) {
+            unlink($destination);
+        }
+        $file                = fopen($destination, 'w+');
+        fwrite($file, $data);
+        fclose($file);
     }
-    $file                = fopen($destination, 'w+');
-    fwrite($file, $data);
-    fclose($file);
 
+    dd('fdas');
     $command             = [
         $patcher_exec,
         '-O',
