6877ddc4820d9f2b041b7841846d400eaa73532e
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-07-20 09:04:49 -0500

    test date

--- www/update.php
+++ www/update.php
@@ -1,12 +1,15 @@
 <?php
+/**
+ * CWP Media tool
+ */
 
-require_once(".config.inc.php");
+require_once '.config.inc.php';
 
 HTMLDisplay::$timeout = 3;
 include_once __LAYOUT_HEADER__;
-$form_number = $_REQUEST['form_number'];
-;
-$media->excelArray($form_number);
-$excel = new MediaXLSX($media, true);
+$form_number          = $_REQUEST['form_number'];
 
-echo HTMLDisplay::JavaRefresh("/form.php?".$_SERVER['QUERY_STRING'], 0);
+$media->excelArray($form_number);
+$excel                = new MediaXLSX($media, true);
+
+echo HTMLDisplay::JavaRefresh('/form.php?'.$_SERVER['QUERY_STRING'], 0);
--- www/updater/updater.php
+++ www/updater/updater.php
@@ -5,60 +5,71 @@
 
 require '../.config.inc.php';
 
+function get_content($URL)
+{
+    $ch   = curl_init();
+    curl_setopt($ch, \CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, \CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, \CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($ch, \CURLOPT_URL, $URL);
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
+
 use Symfony\Component\Process\Process;
 
-$builder_exec    = __ROOT_BIN_DIR__.\DIRECTORY_SEPARATOR.'builder.exe';
-$patcher_exec    = __ROOT_BIN_DIR__.\DIRECTORY_SEPARATOR.'patcher.exe';
+$gitRaw           = 'https://raw.githubusercontent.com/bsodergren/cwp_www/main/www/updater/';
+$updateUrl        = $gitRaw.'version.txt';
+$zip_url          = $gitRaw.'Latest.zip';
+
+$builder_exec     = __ROOT_BIN_DIR__.\DIRECTORY_SEPARATOR.'builder.exe';
+$patcher_exec     = __ROOT_BIN_DIR__.\DIRECTORY_SEPARATOR.'patcher.exe';
 
 define('TITLE', 'Media Updater');
 include_once __LAYOUT_HEADER__;
 
-$version         = file_get_contents('https://raw.githubusercontent.com/bsodergren/cwp_www/main/www/updater/version.txt');
-$current         = file_get_contents(__VERSION_FILE__);
+$version          = get_content($updateUrl);
+$current          = file_get_contents(__VERSION_FILE__);
+
+dump([$updateUrl, $version, $current]);
 
 if ($version > $current) {
-    $zip_url        = 'https://github.com/bsodergren/cwp_www/raw/main/www/updater/Latest.zip';
     echo 'time to update date '.$patcher_exec;
 
-    $ch             = curl_init();
-    curl_setopt($ch, \CURLOPT_URL, $zip_url);
-    curl_setopt($ch, \CURLOPT_RETURNTRANSFER, 1);
-    $data           = curl_exec($ch);
-    curl_close($ch);
+    $data             = get_content($zip_url);
+
     if (!is_dir(__VERSION_DL_DIR__)) {
         mkdir(__VERSION_DL_DIR__, 0777, true);
     }
 
-    $destination    = __VERSION_DL_DIR__.\DIRECTORY_SEPARATOR.'Latest.zip';
+    $destination      = __VERSION_DL_DIR__.\DIRECTORY_SEPARATOR.'Latest.zip';
 
     if (file_exists($destination)) {
         unlink($destination);
     }
-    $file           = fopen($destination, 'w+');
+    $file             = fopen($destination, 'w+');
     fwrite($file, $data);
     fclose($file);
 
-    $command  = [
+    $command          = [
         $patcher_exec,
         '-O',
         __DRIVE_LETTER__.$conf['server']['root_dir'],
         '-P',
         $destination,
-
     ];
-
 
     $process          = new Process($command);
     $process->setTimeout(60000);
 
-    $runCommand = $process->getCommandLine();
+    $runCommand       = $process->getCommandLine();
 
     dd($runCommand);
 
-//            $process->start();
-//            $process->wait($callback);
-
-
+    //            $process->start();
+    //            $process->wait($callback);
 }
 
 include_once __LAYOUT_FOOTER__;
