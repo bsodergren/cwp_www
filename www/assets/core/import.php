352439e68a7572df31cb73d05e09389400f2a94c
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-07-20 13:11:58 -0500

    ff

--- www/assets/core/import.php
+++ www/assets/core/import.php
@@ -1,18 +1,21 @@
 <?php
+/**
+ * CWP Media tool
+ */
 
 require_once '.config.inc.php';
 
 use Nette\Utils\FileSystem;
 
-HTMLDisplay::$timeout = 10;
-HTMLDisplay::$url = 'import.php';
+HTMLDisplay::$timeout  = 10;
+HTMLDisplay::$url      = 'import.php';
 
-$error = false;
+$error                 = false;
 // Store errors here
 $fileExtensionsAllowed = ['pdf'];
 // These will be the only file extensions allowed
 if (isset($_POST['submit'])) {
-    if ($_POST['job_number'] != '') {
+    if ('' != $_POST['job_number']) {
         $job_number = $_POST['job_number'];
     }
 
@@ -23,32 +26,32 @@ if (isset($_POST['submit'])) {
     if (isset($_POST['mail_file'])) {
         list($fullFile, $imap_id) = explode('|', $_POST['mail_file']);
 
-        $fileName = basename($fullFile);
-        $fileSize = filesize($fullFile);
-        $fileTmpName = $fullFile;
+        $fileName                 = basename($fullFile);
+        $fileSize                 = filesize($fullFile);
+        $fileTmpName              = $fullFile;
 
-        $hostname = '{imap.gmail.com:993/imap/ssl}CWP';
-        $username = $conf['gmail']['name'];
-        $password = $conf['gmail']['password'];
+        $hostname                 = '{imap.gmail.com:993/imap/ssl}CWP';
+        $username                 = $conf['gmail']['name'];
+        $password                 = $conf['gmail']['password'];
 
         /* try to connect */
-        $imap = imap_open($hostname, $username, $password) or die('Cannot connect to Gmail: '.imap_last_error());
+        $imap                     = imap_open($hostname, $username, $password) or exit('Cannot connect to Gmail: '.imap_last_error());
     } else {
-        $fileName = $_FILES['the_file']['name'];
-        $fileSize = $_FILES['the_file']['size'];
+        $fileName    = $_FILES['the_file']['name'];
+        $fileSize    = $_FILES['the_file']['size'];
         $fileTmpName = $_FILES['the_file']['tmp_name'];
     }
 
-    if ($fileName == '') {
+    if ('' == $fileName) {
         HTMLDisplay::output("<span class='p-3 text-danger'> no File selected </span> <br>");
         $error = true;
     } else {
-        $f = explode('.', $fileName);
-        $f = end($f);
+        $f             = explode('.', $fileName);
+        $f             = end($f);
         $fileExtension = strtolower($f);
     }
 
-    if (! in_array($fileExtension, $fileExtensionsAllowed)) {
+    if (!in_array($fileExtension, $fileExtensionsAllowed)) {
         HTMLDisplay::output('This file extension is not allowed. Please upload a PDF file<br>');
         $error = true;
     }
@@ -58,29 +61,29 @@ if (isset($_POST['submit'])) {
         $error = true;
     }
 
-    if ($job_number == '') {
+    if ('' == $job_number) {
         HTMLDisplay::output("<span class='p-3 text-danger'>No Job Number </span> <br>");
         $error = true;
     }
     HTMLDisplay::$url = 'import.php';
 
-    if ($error == false) {
-        HTMLDisplay::$url = 'index.php';
+    if (false == $error) {
+        HTMLDisplay::$url     = 'index.php';
         HTMLDisplay::$timeout = 5;
 
-        $media_closing = '/'.basename($fileName, '.pdf');
-        $locations = new MediaFileSystem($fileName, $job_number);
-        $pdf_directory = $locations->getDirectory('pdf', true);
-        $pdf_file = $pdf_directory.'/'.basename($fileName);
+        $media_closing        = '/'.basename($fileName, '.pdf');
+        $locations            = new MediaFileSystem($fileName, $job_number);
+        $pdf_directory        = $locations->getDirectory('pdf', true);
+        $pdf_file             = $pdf_directory.'/'.basename($fileName);
 
         if (file_exists($pdf_file)) {
             FileSystem::delete($pdf_file);
         }
 
-        if (! file_exists($pdf_file)) {
+        if (!file_exists($pdf_file)) {
             $didUpload = move_uploaded_file($fileTmpName, $pdf_file);
 
-            if ($didUpload === false) {
+            if (false === $didUpload) {
                 $didUpload = rename($fileTmpName, $pdf_file);
             }
 
@@ -91,10 +94,10 @@ if (isset($_POST['submit'])) {
                     1 => ['pipe', 'w'],
                 ];
 
-                $qdf_cmd = FileSystem::normalizePath('"'.__ROOT_BIN_DIR__.'/qpdf" ');
-                $pdf_file = FileSystem::normalizePath($pdf_file);
-                $cmd = $qdf_cmd.'"'.$pdf_file.'" '.' --pages . 1-z -- --replace-input ';
-                $process = proc_open($cmd, $descriptorspec, $pipes);
+                $qdf_cmd        = FileSystem::normalizePath('"'.__ROOT_BIN_DIR__.'/qpdf" ');
+                $pdf_file       = FileSystem::normalizePath($pdf_file);
+                $cmd            = $qdf_cmd.'"'.$pdf_file.'"  --pages . 1-z -- --replace-input ';
+                $process        = proc_open($cmd, $descriptorspec, $pipes);
 
                 HTMLDisplay::output('Waiting for PDF for finish <br>');
                 sleep(5);
@@ -107,12 +110,12 @@ if (isset($_POST['submit'])) {
                 }
             } else {
                 HTMLDisplay::output('An error occurred. Please contact the administrator.');
-            } //end if
+            } // end if
         } else {
             HTMLDisplay::output("File already was uploaded<br>\n");
-        } //end if
+        } // end if
 
-        $MediaImport = new MediaImport($pdf_file, $job_number);
+        $MediaImport          = new MediaImport($pdf_file, $job_number);
 
         if ($MediaImport->status < 1) {
             HTMLDisplay::output("<span class='p-3 text-danger'>File failed to process</span> <br>");
