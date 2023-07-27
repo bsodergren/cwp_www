963bb70da331e27a3fb8840c1d06a789a06ae02a
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-07-27 07:04:10 -0500

    fff

--- config/variables.php
+++ config/variables.php
@@ -14,26 +14,18 @@ $__request_name   = basename($__filename, '.php');
 $__script_name    = basename($_SERVER['SCRIPT_NAME'], '.php');
 
 define('__SCRIPT_NAME__', basename($_SERVER['PHP_SELF'], '.php'));
-
-if (__NO_UPDATES__ == true) {
-    define('__NAVBAR_LINKS__', [
-        'Home'         => '/index.php',
-        'Import'       => '/import.php',
-        'Trim Sizes'   => '/settings/trim.php',
-        'Settings'     => [
-            'Language'       => '/settings/language.php',
-            'Local Settings' => '/settings/local.php',
-        ],
-    ]);
-} else {
-    define('__NAVBAR_LINKS__', [
-            'Home'         => '/index.php',
-            'Import'       => '/import.php',
-            'Trim Sizes'   => '/settings/trim.php',
-            'Settings'     => [
-                'Update'         => '/updater/updater.php',
-                'Language'       => '/settings/language.php',
-                'Local Settings' => '/settings/local.php',
-            ],
-        ]);
+$nav_bar_links    = [
+    'Home'         => '/index.php',
+    'Import'       => '/import.php',
+    'Trim Sizes'   => '/settings/trim.php',
+    'Settings'     => [
+        'Paper'          => '/paper.php',
+        'Language'       => '/settings/language.php',
+        'Local Settings' => '/settings/local.php',
+    ],
+];
+if (__NO_UPDATES__ == false) {
+    $nav_bar_links['Settings']['Update']         = '/updater/updater.php';
 }
+
+define('__NAVBAR_LINKS__', $nav_bar_links);
