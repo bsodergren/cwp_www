feed7cc196866279423ece97dc979a367312a9fc
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-08-02 07:04:41 -0500

    ss

--- _upgrade.php
+++ _upgrade.php
@@ -1,10 +1,17 @@
 <?php
+/**
+ * CWP Media tool
+ */
 
 $delete_file = __PUBLIC_ROOT__.\DIRECTORY_SEPARATOR.'delete.txt';
-if(file_exists($delete_file))
-{
-$file = file_get_contents($delete_file);
+if (file_exists($delete_file)) {
+    $file = file_get_contents($delete_file);
 
-dump($file);
+    $fileArray = explode("\n", $file);
 
+    foreach ($fileArray as $file) {
+        if (file_exists(__PUBLIC_ROOT__.\DIRECTORY_SEPARATOR.$file)) {
+            dump(__PUBLIC_ROOT__.\DIRECTORY_SEPARATOR.$file);
+        }
+    }
 }
