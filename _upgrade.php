8ea5fd606a51c566f44b9248a601c9a1d674a0e5
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-08-02 06:49:23 -0500

    gg

--- _upgrade.php
+++ _upgrade.php
@@ -1,3 +1,10 @@
 <?php
 
-dd($this);
+$delete_file = __PUBLIC_ROOT__.\DIRECTORY_SEPARATOR.'delete.txt';
+if(file_exists($delete_file))
+{
+$file = file_get_contents($delete_file);
+
+dump($file);
+
+}
