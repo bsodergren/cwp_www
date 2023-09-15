3374e9337d081107aaecaf51bd9bb18042c90f21
Bjorn Sodergren <bjorn.sodergren@gmail.com[filter lfs]>
2023-09-15 07:58:21 -0500

    Updates to v 2.3.5

--- www/test.php
+++ www/test.php
@@ -3,7 +3,6 @@
  * CWP Media tool for load flags
  */
 
-use CWP\Core\Media;
 use League\Flysystem\DirectoryAttributes;
 use League\Flysystem\FileAttributes;
 use League\Flysystem\Filesystem;
@@ -12,38 +11,34 @@ use League\Flysystem\Local\LocalFilesystemAdapter;
 use Spatie\Dropbox\Client;
 use Spatie\FlysystemDropbox\DropboxAdapter;
 
-/**
- * CWP Media tool.
- */
 require_once '.config.inc.php';
 
-// $adapter = new LocalFilesystemAdapter(
-//     // Determine root directory
-//     __PROJECT_ROOT__
-// );
+$adapter = new LocalFilesystemAdapter(
+    // Determine root directory
+    __PROJECT_ROOT__
+);
 
-//  $appKey = 'm2xqkk0ojabhluo';
-//  $appSecret = 'fcy77exrlrh03g1';
+$appKey = 'm2xqkk0ojabhluo';
+$appSecret = 'fcy77exrlrh03g1';
 
-// $client = new Client(__DROPBOX_AUTH_TOKEN__);
-// $adapter = new DropboxAdapter($client);
-// $filesystem = new Filesystem($adapter);
-// $path = '.';
-// try {
-//     $listing = $filesystem->listContents($path, 0);
-//     /** @var \League\Flysystem\StorageAttributes $item */
-//     foreach ($listing as $item) {
-
-//         $path = $item->path();
-//         if ($item instanceof FileAttributes) {
-//             echo $path.'<br>';
-//             // handle the file
-//         } elseif ($item instanceof DirectoryAttributes) {
-//             // handle the directory
-//             echo $path.'<br>';
-//         }
-//     }
-// } catch (FilesystemException $exception) {
-//     dd($exception);
-//     // handle the error
-// }
+$client = new Client(__DROPBOX_AUTH_TOKEN__);
+$adapter = new DropboxAdapter($client);
+$filesystem = new Filesystem($adapter);
+$path = '.';
+try {
+    $listing = $filesystem->listContents($path, 0);
+    /** @var \League\Flysystem\StorageAttributes $item */
+    foreach ($listing as $item) {
+        $path = $item->path();
+        if ($item instanceof FileAttributes) {
+            echo $path.'<br>';
+            // handle the file
+        } elseif ($item instanceof DirectoryAttributes) {
+            // handle the directory
+            echo $path.'<br>';
+        }
+    }
+} catch (FilesystemException $exception) {
+    dd($exception);
+    // handle the error
+}
