36977dee243d06be5b942edd1e46608b1ea4aa19
Bjorn Sodergren <bjorn.sodergren@gmail.com[filter lfs]>
2023-09-25 07:56:03 -0500

    Updates to v 2.6.3

--- src/App/Filesystem/MediaFileSystem.php
+++ src/App/Filesystem/MediaFileSystem.php
@@ -10,10 +10,9 @@ namespace CWP\Filesystem;
  */
 
 use CWP\Core\Media;
+use Nette\InvalidStateException;
 use Nette\IOException;
 use Nette\Utils\FileSystem;
-use CWP\Filesystem\MediaFinder;
-use Nette\InvalidStateException;
 
 class MediaFileSystem
 {
@@ -32,46 +31,46 @@ class MediaFileSystem
         $this->dropbox = new MediaDropbox();
     }
 
-
-    public  function  getContents($path)
+    public function getContents($path)
     {
-        if(Media::$Dropbox) {
+        if (Media::$Dropbox) {
             $dropbox = new MediaDropbox();
+
             return $dropbox->getContents($path);
         } else {
             $f = new MediaFinder();
-            $array= $f->search($path,'*.pdf');
-            foreach($array as $file){
-                $return[] = ['name'=>basename($file),'path'=>$file];
+            $array = $f->search($path, '*.pdf');
+            foreach ($array as $file) {
+                $return[] = ['name' => basename($file), 'path' => $file];
             }
+
             return $return;
         }
     }
 
+    public static function exists($file)
+    {
+        $fs = new self($file);
 
-public static function exists($file)
-{
-    $fs = new MediaFileSystem($file);
+        if (Media::$Dropbox) {
+            $dropbox = new MediaDropbox();
 
-    if(Media::$Dropbox) {
-        $dropbox = new MediaDropbox();
-        return $dropbox->exists($file);
-    } else {
-        $directory = $fs->getDirectory('upload',true);
-        $pdf_file = $directory . DIRECTORY_SEPARATOR . $file;
-        return file_exists($pdf_file);
+            return $dropbox->exists($file);
+        } else {
+            $directory = $fs->getDirectory('upload', true);
+            $pdf_file = $directory.\DIRECTORY_SEPARATOR.$file;
 
-    }
-}
-    public static function uploadFile($filename, $dropboxFilename, $options = []){
-
-
-          if(Media::$Dropbox) {
-            MediaDropbox::UploadFile($filename, $dropboxFilename,$options);
+            return file_exists($pdf_file);
         }
-
-
     }
+
+    public static function uploadFile($filename, $dropboxFilename, $options = [])
+    {
+        if (Media::$Dropbox) {
+            MediaDropbox::UploadFile($filename, $dropboxFilename, $options);
+        }
+    }
+
     public function getFilename($type = '', $form_number = '', $create_dir = '')
     {
         return $this->__filename($type, $form_number, $create_dir);
@@ -112,8 +111,8 @@ public static function exists($file)
         $filename = $directory.\DIRECTORY_SEPARATOR.$filename;
         $filename = FileSystem::normalizePath($filename);
         if ('pdf' == strtolower($type)) {
-
         }
+
         return $filename;
     }
 
@@ -123,48 +122,42 @@ public static function exists($file)
 
         if (false !== $this->__filename()) {
             $output_filename = $this->__filename();
-            $output_filename = str_replace($this->job_number.'_','',$output_filename);
-            $output_filename = $this->job_number.$output_filename ;
+            $output_filename = str_replace($this->job_number.'_', '', $output_filename);
+            $output_filename = $this->job_number.$output_filename;
         }
 
         $directory = $output_filename;
-        if(Media::$Dropbox) {
-            if(__DROPBOX_FILES_DIR__ != ''){
+        if (Media::$Dropbox) {
+            if (__DROPBOX_FILES_DIR__ != '') {
                 $directory = __DROPBOX_FILES_DIR__.\DIRECTORY_SEPARATOR.$output_filename;
             }
         }
         if ('xlsx' == strtolower($type)) {
             $directory .= \DIRECTORY_SEPARATOR.__XLSX_DIRECTORY__;
-            if(Media::$Dropbox) {
-
-                $create_dir = true;
+            if (Media::$Dropbox) {
+                $create_dir = false;
             }
         }
 
         if ('zip' == strtolower($type)) {
-
-             $directory .= \DIRECTORY_SEPARATOR.__ZIP_DIRECTORY__;
+            $directory .= \DIRECTORY_SEPARATOR.__ZIP_DIRECTORY__;
             $create_dir = true;
-
         }
         if ('upload' == strtolower($type)) {
-
-            $directory = DIRECTORY_SEPARATOR.'Uploads';
-           $create_dir = true;
-
-       }
-       if(!Media::$Dropbox) {
-        if (\defined('__MEDIA_FILES_DIR__')) {
-            if (__MEDIA_FILES_DIR__ != '' ) {
-                if (!is_dir(__MEDIA_FILES_DIR__)) {
-                    FileSystem::createDir(__MEDIA_FILES_DIR__, 511);
+            $directory = \DIRECTORY_SEPARATOR.'Uploads';
+            $create_dir = true;
+        }
+        if (!Media::$Dropbox) {
+            if (\defined('__MEDIA_FILES_DIR__')) {
+                if (__MEDIA_FILES_DIR__ != '') {
+                    if (!is_dir(__MEDIA_FILES_DIR__)) {
+                        FileSystem::createDir(__MEDIA_FILES_DIR__, 511);
+                    }
+                    $directory = __MEDIA_FILES_DIR__.\DIRECTORY_SEPARATOR.$directory;
                 }
-                $directory = __MEDIA_FILES_DIR__.DIRECTORY_SEPARATOR.$directory;
             }
         }
-    }
-    if ('pdf' == strtolower($type)) {
-
+        if ('pdf' == strtolower($type)) {
         }
 
         $directory = FileSystem::unixSlashes($directory);
@@ -184,8 +177,7 @@ public static function exists($file)
 
     public function getDropboxDirectory($type = 'upload', $create_dir = false)
     {
-        if(Media::$Dropbox) {
-
+        if (Media::$Dropbox) {
             $this->directory = $this->dropbox->getDirectory($type, $create_dir);
         } else {
             $this->directory = $this->getDirectory($type, $create_dir);
@@ -214,16 +206,14 @@ public static function exists($file)
 
     public static function delete($file)
     {
-
         $msg = null;
         if (__USE_DROPBOX__ == true) {
             $d = new MediaDropbox();
 
             $msg = $d->deleteFile($file);
 
-           return $msg;
+            return $msg;
         }
-
 
         if (file_exists($file) || is_dir($file)) {
             try {
