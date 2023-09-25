0e0e1f246a7f77be38da58015b90dcb504b6cf1e
Bjorn Sodergren <bjorn.sodergren@gmail.com[filter lfs]>
2023-09-25 15:31:42 -0500

    Updates to v 2.6.6

--- src/App/Template/Desktop/Footer.php
+++ src/App/Template/Desktop/Footer.php
@@ -5,6 +5,7 @@
 
 namespace CWP\Template\Desktop;
 
+use CWP\Core\MediaSettings;
 use CWP\Template\HTMLDocument;
 use CWP\Template\Template;
 
@@ -14,6 +15,15 @@ class Footer extends HTMLDocument
 
     public static function display($template = '', $params = [])
     {
+        if (MediaSettings::isSet('__FOOTER_NAV_HTML__')) {
+            $params['FOOT_NAV_PANEL'] = Template::GetHTML(
+                'base/footer/settings_nav',
+                ['FOOTER_NAV_HTML' => __FOOTER_NAV_HTML__,
+                'VERSIONS_HTML' => self::_footerVersionUpdates(),
+                ]
+            );
+        }
+
         $params['END_JAVASCRIPT'] = Template::GetHTML('base/footer/javascript');
         echo Template::GetHTML('base/footer/footer', $params);
     }
