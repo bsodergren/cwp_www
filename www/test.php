c4387d35f02d047f18e97826fcf92a36a7db9ade
Bjorn Sodergren <bjorn.sodergren@gmail.com[filter lfs]>
2023-10-28 04:43:43 -0500

    Updates to v 2.7.8

--- www/test.php
+++ www/test.php
@@ -8,6 +8,18 @@ use CWP\Utils\MediaDevice;
 
 require_once '.config.inc.php';
 
+$client   = new \Google\Client();
+$client->setClientId('882775659043-hc67vibec4eeio5bkb1t5mdnlk1nkeju.apps.googleusercontent.com');
+$client->setClientSecret('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');
+$client->refreshToken('1//05PW3oMgnMfnwCgYIARAAGAUSNwF-L9IrWsQ6cqqq0TauLTQxBTOn63BonmoFRoFgTbHRHYLNagRbTXb6fhTNO67BmQ0-RBaa9ww');
+$client->setApplicationName('plexmediabackupserver');
+
+$service  = new \Google\Service\Drive($client);
+$adapter  = new \Masbug\Flysystem\GoogleDriveAdapter($service, 'plex_backups');
+$fs       = new \League\Flysystem\Filesystem($adapter);
+$contents = $fs->listContents('', false /* is_recursive */)->toarray();
+dd($contents);
+
 MediaDevice::getHeader();
 
 Template::echo('test/main');
