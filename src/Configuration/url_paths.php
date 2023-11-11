10039f9e82019fafc1879d1d79804705e5d158b3
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-11-10 19:55:25 -0600

    Updates to v 2.8.0

--- src/Configuration/url_paths.php
+++ src/Configuration/url_paths.php
@@ -8,3 +8,5 @@ define('__URL_LAYOUT__', __URL_HOME__.'/assets/themes');
 define('__UPDATER_VERSION__', __URL_HOME__.'/updater/version.php');
 define('__UPDATER_REFRESH__', 60 * 100);
 define('__CURRENT_URL__', __URL_HOME__.'/index.php');
+
+
