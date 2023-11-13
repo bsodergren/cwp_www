77e63de68de18c872a84fffee002e9e4fb9ff755
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-11-13 04:31:42 -0600

    Updates to v 3.1.7

--- www/updater/update.php
+++ www/updater/update.php
@@ -12,20 +12,22 @@ define('TITLE', 'Media Updater');
 use CWP\Utils\MediaDevice;
 
 MediaDevice::getHeader();
+
 echo HTMLDisplay::ProgressBar('start');
+
+$timeout = 20;
 
 if (null === Media::$VersionUpdate) {
     HTMLDisplay::put('Checking for Composer Updates', 'Red');
     Media::$MediaAppUpdater->composerUpdate();
-    HTMLDisplay::put('All up  to date', 'Red');
-    $timeout = 5;
 } else {
-    $timeout = 20;
     Media::$MediaAppUpdater->getUpdate();
     Media::$MediaAppUpdater->composerUpdate();
 }
 
+HTMLDisplay::put('All up  to date', 'Red');
 echo HTMLDisplay::ProgressBar($timeout);
+
 echo HTMLDisplay::JavaRefresh('/index.php', $timeout);
 
 MediaDevice::getFooter();
