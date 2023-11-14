af62c28e5f7cb2743f8f25a00e18cb3e8b11e5c7
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-11-14 05:31:33 -0600

    Updates to v 3.2.7

--- src/App/Utils/Zip.php
+++ src/App/Utils/Zip.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * CWP Media tool for load flags
+ * CWP Media Load Flag Creator
  */
 
 namespace CWP\Utils;
@@ -26,12 +26,12 @@ class AdvancedFilesystemIterator extends \ArrayIterator
 
     public function sort($method)
     {
-        if (! method_exists('SplFileInfo', $method)) {
+        if (!method_exists('SplFileInfo', $method)) {
             throw new \InvalidArgumentException(sprintf('Method "%s" does not exist in SplFileInfo', $method));
         }
 
         $this->uasort(function (\SplFileInfo $a, \SplFileInfo $b) use ($method) {
-            return \is_string($a->$method()) ? strnatcmp($a->$method(), $b->$method()) : $b->$method() - $a->$method();
+            return \is_string($a->{$method}()) ? strnatcmp($a->{$method}(), $b->{$method}()) : $b->{$method}() - $a->{$method}();
         });
 
         return $this;
@@ -57,47 +57,41 @@ class Zip
     public $remote_xlsx;
     public $remote_zip;
 
-
-    public object $driver ;
-
+    public object $driver;
 
     public function __construct($object)
     {
-        $this->remote_xlsx  = $object->media->xlsx_directory;
-        $this->remote_zip  = $object->media->zip_file;
+        $this->remote_xlsx = $object->media->xlsx_directory;
+        $this->remote_zip = $object->media->zip_file;
         // $this->zip_file  = $object->media->zip_file;
         $this->job_id = $object->job_id;
 
-
         $this->driver = Media::getFileDriver();
 
-        $this->xlsx_dir  = $this->driver->getXLSXDir($this->remote_xlsx);
+        $this->xlsx_dir = $this->driver->getXLSXDir($this->remote_xlsx);
 
-        $this->zip_file = $this->driver->getZipFile($object->media->zip_file, dirname($this->xlsx_dir));
+        $this->zip_file = $this->driver->getZipFile($object->media->zip_file, \dirname($this->xlsx_dir));
 
         // dd($this->xlsx_dir, $this->zip_file);
-
     }
 
     public function zip()
     {
-        $rootPath = realpath($this->xlsx_dir);
-        //$zipPath  = dirname($this->zip_file);
+        // $zipPath  = dirname($this->zip_file);
 
         $this->driver->downloadXLSXFiles($this->remote_xlsx);
 
-
-        $zip      = new \ZipArchive();
+        $zip = new \ZipArchive();
         $zip->open($this->zip_file, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
 
-        $files    = (new AdvancedFilesystemIterator($rootPath))->sortByMTime();
+        $files = (new AdvancedFilesystemIterator($this->xlsx_dir))->sortByMTime();
 
         foreach ($files as $name => $file) {
             // Skip directories (they would be added automatically)
-            if (! $file->isDir()) {
+            if (!$file->isDir()) {
                 // Get real and relative path for current file
-                $filePath     = $file->getRealPath();
-                $relativePath = substr($filePath, \strlen($rootPath) + 1);
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, \strlen($this->xlsx_dir) + 1);
                 // Add current file to archive
                 $zip->addFile($filePath, $relativePath);
             }
@@ -106,17 +100,13 @@ class Zip
         // Zip archive will be created only after closing object
         $d = $zip->close();
 
-
         if (true === $d) {
             $this->driver->save($this->zip_file, $this->remote_zip);
-
-
-           $table = Media::$explorer->table('media_job')->where('job_id', $this->job_id)->update(['zip_exists' => 1]);
-
-            //FileSystem::delete(dirname($rootPath, 3));
+            $table = Media::$explorer->table('media_job')->where('job_id', $this->job_id)->update(['zip_exists' => 1]);
+            FileSystem::delete(\dirname($this->xlsx_dir, 1));
             return 'Zip file created';
 
-            //FileSystem::delete(dirname($rootPath));
+
         }
 
         return 'zip file not created, probably a file open';
@@ -124,16 +114,16 @@ class Zip
 
     public function exportZip($pdf_file, $jsonFile)
     {
-        $zipPath  = \dirname($pdf_file, 2).'/Export';
+        $zipPath = \dirname($pdf_file, 2).'/Export';
         FileSystem::createDir($zipPath);
 
         $zip_file = $zipPath.'/export.zip';
-        $zip      = new \ZipArchive();
+        $zip = new \ZipArchive();
         $zip->open($zip_file, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
 
         $zip->addFile($pdf_file, basename($pdf_file));
         $zip->addFile($jsonFile, basename($jsonFile));
 
-        $d        = $zip->close();
+        $d = $zip->close();
     }
 }
