4ec9374a77b71e62812f8e0cbd16fc852f732698
bjorn sodergren <bjorn.sodergren@gmail.com>
2024-01-12 01:10:23 -0600

    Updates to v 3.8.3

--- www/updater/version.php
+++ www/updater/version.php
@@ -4,6 +4,7 @@
  */
 
 use CWP\Template\Template;
+use CWP\Utils\MediaDevice;
 use CWP\Template\HTMLDocument;
 use CWP\Updater\MediaAppUpdater;
 
@@ -12,8 +13,13 @@ define('NO_DB_CHECK', true);
 define('__PROJECT_ROOT__', dirname(__FILE__, 4));
 define('__PUBLIC_ROOT__', dirname(__FILE__, 3));
 define('__HTTP_ROOT__', dirname(__FILE__, 2));
-
+define('__LAYOUT_DIR__', \DIRECTORY_SEPARATOR.'assets');
+define('__LAYOUT_ROOT__', __HTTP_ROOT__.__LAYOUT_DIR__);
+define('__THEME_DIR__', __LAYOUT_ROOT__.\DIRECTORY_SEPARATOR.'themes');
 require __PUBLIC_ROOT__.\DIRECTORY_SEPARATOR.'bootstrap.php';
+
+(new MediaDevice())->run();
+define('__DEVICE__', MediaDevice::$DEVICE);
 
 $appUpdate = new MediaAppUpdater();
 
