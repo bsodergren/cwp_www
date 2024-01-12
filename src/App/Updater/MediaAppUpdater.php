27020d7a368afd6ad700ed161af4bd733a5f3ebe
bjorn sodergren <bjorn.sodergren@gmail.com>
2024-01-12 01:13:35 -0600

    Updates to v 3.8.5

--- src/App/Updater/MediaAppUpdater.php
+++ src/App/Updater/MediaAppUpdater.php
@@ -35,6 +35,7 @@ class MediaAppUpdater
         curl_setopt($ch, \CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, \CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, \CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
         curl_setopt($ch, \CURLOPT_URL, $URL);
         $data = curl_exec($ch);
         curl_close($ch);
