2909c1e2374c251bf19afa4994b3c1dbf45f93a6
bjorn sodergren <bjorn.sodergren@gmail.com>
2024-02-27 19:53:32 -0600

    Updates to v 4.3.1

--- src/App/Process/MediaProcess.php
+++ src/App/Process/MediaProcess.php
@@ -24,60 +24,68 @@ class MediaProcess
 
     public static function Check($media)
     {
-        if (
-            array_key_exists('FORM_PROCESS', $_REQUEST)
-            || array_key_exists('submit', $_REQUEST)
-            || array_key_exists('update_job', $_REQUEST)
-        ) {
-            $refer_script = basename(parse_url($_SERVER['HTTP_REFERER'], \PHP_URL_PATH), '.php');
+        $refer_script = basename(parse_url($_SERVER['HTTP_REFERER'], \PHP_URL_PATH), '.php');
 
-            if (__SCRIPT_NAME__ == $refer_script) {
-                MediaError::msg('info', $refer_script.'< >'.__SCRIPT_NAME__, 0);
-            }
-
-            if (null === $refer_script || '' == $refer_script) {
-                MediaError::msg('info', 'referer not set', 0);
-                echo HTMLDisplay::JavaRefresh('/index.php', 0);
-            }
-
-            define('__FORM_POST__', $refer_script);
-
-            if (isset($_POST['divClass'])) {
-                list($k, $id) = explode('_', $_POST['row_id']);
-                if (str_contains($_POST['divClass'], 'show')) {
-                    $hidden = 1;
-                } else {
-                    $hidden = 0;
-                }
-
-                $count = Media::$explorer->table('media_job') // UPDATEME
-                    ->where('job_id', $id) // must be called before update()
-                    ->update([
-                        'hidden' => $hidden,
-                    ])
-                ;
-                exit;
-            }
-
-            $procesClass = ucfirst(__FORM_POST__);
-            if (array_key_exists('FORM_PROCESS', $_REQUEST)) {
-                switch ($_REQUEST['FORM_PROCESS']) {
-                    case 'updateSetting':
-                        $procesClass = ucfirst('Settings');
-                        break;
-                    case 'createJob':
-                        $procesClass = ucfirst('createJob');
-                        break;
-                }
-            }
-
-            $procesClass = 'CWP\\Process\\'.$procesClass;
-
-            $mediaProcess = new $procesClass($media);
-            $mediaProcess->run($_REQUEST);
-
-            $mediaProcess->reload();
+        if (__SCRIPT_NAME__ == $refer_script) {
+            MediaError::msg('info', $refer_script.'< >'.__SCRIPT_NAME__, 0);
         }
+
+        if (null === $refer_script || '' == $refer_script) {
+            MediaError::msg('info', 'referer not set', 0);
+            echo HTMLDisplay::JavaRefresh('/index.php', 0);
+        }
+
+        define('__FORM_POST__', $refer_script);
+
+        if (isset($_POST['divClass'])) {
+            list($k, $id) = explode('_', $_POST['row_id']);
+            if (str_contains($_POST['divClass'], 'show')) {
+                $hidden = 1;
+            } else {
+                $hidden = 0;
+            }
+
+            $count = Media::$explorer->table('media_job') // UPDATEME
+                ->where('job_id', $id) // must be called before update()
+                ->update([
+                    'hidden' => $hidden,
+                ])
+            ;
+            exit;
+        }
+
+        $procesClass = str_replace(' ', '_', ucwords(str_replace('_', ' ', __FORM_POST__)));
+        if (array_key_exists('FORM_PROCESS', $_REQUEST)) {
+            switch ($_REQUEST['FORM_PROCESS']) {
+                case 'updateSetting':
+                    $procesClass = ucfirst('Settings');
+                    break;
+                case 'createJob':
+                    $procesClass = ucfirst('createJob');
+                    break;
+                case 'updateEmail':
+                case 'addEmail':
+                    $procesClass = ucfirst('emailList');
+                    break;
+            }
+        }
+        if (array_key_exists('FORM_PROCESS', $_REQUEST)) {
+            switch ($_REQUEST['FORM_PROCESS']) {
+                case 'updateSetting':
+                    $procesClass = ucfirst('Settings');
+                    break;
+                case 'createJob':
+                    $procesClass = ucfirst('createJob');
+                    break;
+            }
+        }
+
+        $procesClass = 'CWP\\Process\\'.$procesClass;
+
+        $mediaProcess = new $procesClass($media);
+        $mediaProcess->run($_REQUEST);
+
+        $mediaProcess->reload();
 
         return null;
     }
--- www/process.php
+++ www/process.php
@@ -3,62 +3,10 @@
  * CWP Media Load Flag Creator.
  */
 
-use CWP\Core\MediaError;
-use CWP\HTML\HTMLDisplay;
+use CWP\Process\MediaProcess;
 
 define('PROCESS', true);
 
 require '.config.inc.php';
 
-$refer_script = basename(parse_url($_SERVER['HTTP_REFERER'], \PHP_URL_PATH), '.php');
-
-if (__SCRIPT_NAME__ == $refer_script) {
-    MediaError::msg('info', $refer_script.'< >'.__SCRIPT_NAME__, 0);
-}
-
-if (null === $refer_script || '' == $refer_script) {
-    MediaError::msg('info', 'referer not set', 0);
-    echo HTMLDisplay::JavaRefresh('/index.php', 0);
-}
-
-define('__FORM_POST__', $refer_script);
-
-if (isset($_POST['divClass'])) {
-    list($k, $id) = explode('_', $_POST['row_id']);
-    if (str_contains($_POST['divClass'], 'show')) {
-        $hidden = 1;
-    } else {
-        $hidden = 0;
-    }
-
-    $count = $explorer->table('media_job') // UPDATEME
-        ->where('job_id', $id) // must be called before update()
-        ->update([
-            'hidden' => $hidden,
-        ])
-    ;
-    exit;
-}
-
-$procesClass = str_replace(' ', '_', ucwords(str_replace('_', ' ', __FORM_POST__)));
-if (array_key_exists('FORM_PROCESS', $_REQUEST)) {
-    switch ($_REQUEST['FORM_PROCESS']) {
-        case 'updateSetting':
-            $procesClass = ucfirst('Settings');
-            break;
-        case 'createJob':
-            $procesClass = ucfirst('createJob');
-            break;
-        case 'updateEmail':
-        case 'addEmail':
-            $procesClass = ucfirst('emailList');
-            break;
-    }
-}
-
-$procesClass = 'CWP\\Process\\'.$procesClass;
-
-$mediaProcess = new $procesClass($media);
-$mediaProcess->run($_REQUEST);
-
-$mediaProcess->reload();
+MediaProcess::Check($media);
